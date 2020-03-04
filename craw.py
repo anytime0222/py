2b93e7da3726ceadabe47e842480dcf753766c27
Anytime <anyitme0222@gmail.com>
2020-03-04 17:53:15 +0900

    1번

--- craw.py
+++ craw.py
@@ -39,11 +39,29 @@ for page in range(1,2):
 
         #페이지 이동하면서 크롤링
         #크롤링 할 때 데이터 확인하기.
-        boxItems = driver.find_elements_by_css_selector('.oTravelBox>.boxList>.li')
+        
+        try:
+            element = WebDriverWait(driver,10).until(
+                EC.presence_of_all_elements_located( (By.CLASS_NAME, 'oTravelBox'))
+
+            )
+        except Exception as e:
+            print('오류발생',e)
+            
+        boxItems = driver.find_elements_by_css_selector('.oTravelBox>.boxList>li')
         
         for li in boxItems:
-            print('상품명 :' ,li.find_element_by_css_selector('h5.proTit').text)
 
+            print('썸네일 : ' ,li.find_element_by_css_selector('img').get_attribute('src'))
+            print('링크  : ' ,li.find_element_by_css_selector('a').get_attribute('onclick'))
+            print('상품명 : ' , li.find_element_by_css_selector('h5.proTit').text)
+            print('코멘트 : ' , li.find_element_by_css_selector('p.proSub').text)
+            print('기간 : ' , li.find_element_by_css_selector('p.proInfo').text)
+
+            for info in li.find_elements_by_css_selector('.info-row .proInfo'):
+                print( info.text)
+
+            print('------------------------------------------')
 
     except Exception as e1:
         print('오류', e1)
